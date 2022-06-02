06252c47b18722f8cd45cf9fbafdbc7b3778d24c
Guilherme Rollzing Schineider <56838656+Guirollzing@users.noreply.github.com>
2022-06-02 18:50:02 -0300

    Principal

---
+++ Modules/Modules.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+
+namespace BotDoGui.Modules
+{
+    public class Commands : ModuleBase<SocketCommandContext>
+    {
+        [Command("Felipe")]
+        public async Task Ping()
+        {
+            ReplyAsync("Tabaia");
+        }
+
+      
+    }
+}
